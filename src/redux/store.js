e4c8e32ee8fc089683e04090db6864ca552c22d5
Merrick Kavolsky <technicmk@gmail.com>
2017-04-12 00:13:37 +0200

    Remove redux-promise middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
 
-const enhancer = applyMiddleware(promise(), thunk, logger());
+const enhancer = applyMiddleware(thunk, logger());
 
 export default createStore(reducer, enhancer);
 
