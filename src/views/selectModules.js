5fb238072e323e87c8591282f7377232cb3da6c2
Merrick Kavolsky <technicmk@gmail.com>
2017-04-12 00:32:21 +0200

    Create Stub SelectModulesView

---
+++ src/views/selectModules.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SelectModulesView extends React.Component{
+  render(){
+    return (
+      <div>Select Modules</div>
+    );
+  }
+}
+
+export default SelectModulesView;
