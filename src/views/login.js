8093eafcbac2bce0f01a1fc5eba0ad4c5843eb56
Merrick Kavolsky <technicmk@gmail.com>
2017-04-12 00:15:37 +0200

    Create user login view

--- src/views/login.js
+++ src/views/login.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux';
+
+import {actionCreator as userAction} from '../redux/userReducer';
 
 
-export default class LoginView extends React.Component{
+const mapStateToProps = (state) => ({
+  user: state.user
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+ login : userAction.login
+}, dispatch);
+
+
+@connect(mapStateToProps, mapDispatchToProps)
+class LoginView extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+  }
+
+  handleLoginClick(){
+    let email = this.tfEmail.getValue();
+    let password= this.tfPassword.getValue();
+    this.props.login(email, password);
+  }
 
   render() {
     return (
       <div>
         <h2>Log In</h2>
-        <RaisedButton primary={true} label="Click Me" />
+        <div>
+          <TextField
+            floatingLabelText="Email"
+            ref={el => this.tfEmail = el}
+          /><br/>
+
+          <TextField
+            floatingLabelText="Password"
+            type="password"
+            ref={el => this.tfPassword = el}
+          /><br/>
+        </div>
+        <RaisedButton primary={true} label="Log In" 
+          onTouchTap={this.handleLoginClick}/>
       </div>
     );
   }
 
 };
 
+export default LoginView;
